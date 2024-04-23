374bd91629daa23d66fee9981bd7b2d9dc356721
Luiscerda <55934823+Luiscerda@users.noreply.github.com>
2024-04-23 13:53:11 -0500

    scrollView update

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,19 +1,21 @@
 ﻿using DancellApp.Services;
 using DancellApp.ViewModels;
 using DancellApp.Views;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
 
 namespace DancellApp;
 
-public partial class App : Application
+public partial class App : Microsoft.Maui.Controls.Application
 {
 	#region Properties
 	public static NavigationPage Navigation { get; internal set; }
 	public static MasterPage Master { get; internal set; }
-    public new static App Current => (App)Application.Current;
+    public new static App Current => (App)Microsoft.Maui.Controls.Application.Current;
     public IServiceProvider Services { get; }
     #endregion
     public App()
 	{
+        App.Current.On<Microsoft.Maui.Controls.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
         var services = new ServiceCollection();
         Services = ConfigureServices(services);
         InitializeComponent();
