9717f1835e570545f38ab6362344d0738a01e2ef
Luiscerda <55934823+Luiscerda@users.noreply.github.com>
2024-05-09 13:51:52 -0500

    View types comitions

--- ViewModels/ComitionViewModels.cs
+++ ViewModels/ComitionViewModels.cs
@@ -24,15 +24,20 @@ namespace DancellApp.ViewModels
             Comitions = new ObservableCollection<ComitionModel>();
             IsEnabled = true;
             IsRunning = false;
+            IsVisible = false;
+            IsVisibleType = false;
             GetComitionByIdPosCommand = new Command(() => GetComitionByIdPos());
         }
         #endregion
 
         #region Attributes
         private string idPos;
+        private string valorRestante;
         private ObservableCollection<ComitionModel> comitions;
         private bool isEnabled;
         private bool isRunning;
+        private bool isVisible;
+        private bool isVisibleType;
         #endregion
 
         #region Properties
@@ -41,10 +46,25 @@ namespace DancellApp.ViewModels
             get => idPos;
             set => SetValue(ref idPos, value);
         }
+        public string ValorRestante
+        {
+            get => valorRestante;
+            set => SetValue(ref valorRestante, value);
+        }
         public bool IsEnabled
         {
             get => isEnabled;
             set => SetValue(ref isEnabled, value);
+        }
+        public bool IsVisible
+        {
+            get => isVisible;
+            set => SetValue(ref isVisible, value);
+        }
+        public bool IsVisibleType
+        {
+            get => isVisibleType;
+            set => SetValue(ref isVisibleType, value);
         }
         public ICommand GetComitionByIdPosCommand { get; }
         public ObservableCollection<ComitionModel> Comitions
@@ -52,13 +72,12 @@ namespace DancellApp.ViewModels
             get => comitions;
             set => SetValue(ref comitions, value);
         }
-
         public bool IsRunning
         {
             get => isRunning;
             set => SetValue(ref isRunning, value);
         }
-
+        public ICommand SelectComitionCommand => new Command<ComitionModel>(ViewTypeComitions);
         #endregion
 
         #region Methods
@@ -85,6 +104,7 @@ namespace DancellApp.ViewModels
                     "OK");
                 return;
             }
+            Comitions.Clear();
             var user = baseConstants.GetUserAsync();
             var result = await this.comitionService.GetComitionByIdPos("/Liquidaciones/GetGestionPrepago", user.UserName, user.Password, IdPos);
             if (result.Is_Error)
@@ -104,7 +124,7 @@ namespace DancellApp.ViewModels
                 IsEnabled = true;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
-                    $"No se encontro comision para el IDPOS {IdPos}.",
+                    $"No se encontro comisión para el IDPOS {IdPos}.",
                     "OK");
                 return;
             }
@@ -116,10 +136,17 @@ namespace DancellApp.ViewModels
                     this.comitions.Add(item);
                 }
             }
-
+            IsVisible = true;
             Comitions = this.comitions;
             IsEnabled = true;
             IsRunning = false;
+        }
+
+        public async void ViewTypeComitions(ComitionModel comition)
+        {
+            IsVisible = false;
+            IsVisibleType = true;
+            ValorRestante = comition.Valor;
         }
         #endregion
     }
